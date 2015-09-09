56cc230d702cdb4f097d6934afda5f36f80188d9
Joel Noguera <noguera.sistemas@gmail.com>
2015-09-09 18:37:41 -0300

    Base para trabajar con TCP/IP

--- TP2_modbus_tcp_ip/main.py
+++ TP2_modbus_tcp_ip/main.py
@@ -32,6 +32,29 @@ def manage_error( packet ):
     else:
         print "Transmission error"
 
+def manage_error_tcp( packet ):
+    print "The value wasnt assigned"
+    if packet[2] == "\x01" :
+        print "ILLEGAL FUNCTION CODE"
+    elif packet[2] == "\x02":
+        print "ILLEGAL DATA ADDRESS"
+    elif packet[2] == "\x03":
+        print "ILLEGAL DATA VALUE"
+    elif packet[2] == "\x04":
+        print "SERVER FAILURE"
+    elif packet[2] == "\x05":
+        print "ACKNOWLEDGE"
+    elif packet[2] == "\x06" :
+        print "SERVER BUSY"
+    elif packet[2] == "\x0A" :
+        print "GATEWAY PROBLEM"
+    elif packet[2] == "\x0B" :
+        print "GATEWAY PROBLEM"
+    else :
+        print "Other error"
+
+
+
 def send_value( pckt ) :
     print "Writing value 0x%s in address 0x%s" %(reg_value, start_addr)
     respond = ""
@@ -50,7 +73,23 @@ def send_value( pckt ) :
     else:
             print "No response"
 
-
+def send_value_tcp( pckt ) :
+    print "Writing value 0x%s in address 0x%s" %(reg_value, start_addr)
+    respond = ""
+    count = ser.timeout -1
+    while (count >= 0  and sys.getsizeof(respond) == 21  ) :   #campiar para tcp
+        print "Sending: " + print_hex(pckt)
+        ser.write(pckt)     #campiar para tcp
+        respond = ser.read(8)    #campiar para tcp
+        count -= 1
+    print "Received : " + print_hex(respond)
+    if sys.getsizeof(respond) > 21:    #campiar para tcp
+        if "\x86" == respond[1]:    #campiar para tcp
+            manage_error(respond)
+        elif respond[len(respond)-2:] == struct.pack("H", calculate_crc16(respond[:len(respond)-2])):  # no usar mas crc
+            print "The value was assigned successfully"
+    else:
+            print "No response"
 
 def recv_multiple_values ( str):
     print "Asking value of %s register(s) from address 0x%s" %(quantity, start_addr)
@@ -65,7 +104,7 @@ def recv_multiple_values ( str):
     if sys.getsizeof(respond) > 26 :
         if respond[len(respond)-2:] == struct.pack("H", calculate_crc16(respond[:len(respond)-2])):
             for i in range(0,int(quantity)):
-                print "[%s]\t=>\t%s\t=>\t" %(value_print * 125 + i + 1 + index_reg , print_hex_without_colon(respond[3+i*2:5+i*2])),
+                print "[%s]\t=>\t%s\t=>\t" %(value_print * 125 + i + index_reg , print_hex_without_colon(respond[3+i*2:5+i*2])),
                 print "%s\t=>\t" %struct.unpack('!H', respond[3+i*2:5+i*2]),
                 print ' '.join('{0:08b}'.format(ord(x), 'b') for x in respond[3+i*2:5+i*2])
         else :
@@ -75,6 +114,28 @@ def recv_multiple_values ( str):
     elif sys.getsizeof(respond) == 21 :
         print "No response"
 
+def recv_multiple_values_tcp ( str):
+    print "Asking value of %s register(s) from address 0x%s" %(quantity, start_addr)
+    respond = ""
+    count = ser.timeout - 1
+    while (count >= 0  and sys.getsizeof(respond) == 21  ) :
+        print "Sending: " + print_hex(str)
+        ser.write(str)   #cambiar para tcp
+        respond = ser.read(5 + int(quantity)*2 )  #cambiar para tcp
+        count -= 1
+    print "Received : " + print_hex(respond)
+    if sys.getsizeof(respond) > 26 :   #cambiar para tcp
+        if respond[len(respond)-2:] == struct.pack("H", calculate_crc16(respond[:len(respond)-2])):   #no usar mas crc
+            for i in range(0,int(quantity)):
+                print "[%s]\t=>\t%s\t=>\t" %(value_print * 125 + i + index_reg , print_hex_without_colon(respond[3+i*2:5+i*2])),
+                print "%s\t=>\t" %struct.unpack('!H', respond[3+i*2:5+i*2]),
+                print ' '.join('{0:08b}'.format(ord(x), 'b') for x in respond[3+i*2:5+i*2])
+        else :
+            print "Transmission error"
+    elif sys.getsizeof(respond) == 26 :  #cambiar para tcp
+        manage_error(respond)
+    elif sys.getsizeof(respond) == 21 :   #cambiar para tcp
+        print "No response"
 
 def write_multiple_values ( str ) :
     print "Writing 0x%s registers in address from 0x%s" %(int(reg_quantity), start_addr)
@@ -99,6 +160,28 @@ def write_multiple_values ( str ) :
     else:
         print "No response"
 
+def write_multiple_values_tcp ( str ) :
+    print "Writing 0x%s registers in address from 0x%s" %(int(reg_quantity), start_addr)
+    respond = ""
+    count = ser.timeout - 1
+    while (count >= 0  and sys.getsizeof(respond) == 21  ) :  # cambiar para tcp
+        print "Sending: " + print_hex(str)
+        ser.write(str)  #cambiar para tcp
+        respond = ser.read(8)   #cambiar para tcp
+        count -= 1
+
+    print "Received : " + print_hex(respond)
+
+    if sys.getsizeof(respond) >21:    #campiar para tcp
+        if respond[len(respond)-2:] == struct.pack("H", calculate_crc16(respond[:len(respond)-2])) :   #no usar mas crc
+            if "\x90" == respond[1]:    #campiar para tcp
+                manage_error(respond)
+            else :
+                print "The values were assigned successfully"
+        else :
+            print "Transmission error"
+    else:
+        print "No response"
 
 def calculate_crc16( str ) :
     crc = 0xFFFF
@@ -125,21 +208,101 @@ reply = easygui.buttonbox(msg,title, choices=choices)
 
 if reply == "TCP/IP" :
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("127.0.0.1", 502))
+    msg = "Setting TCP/IP"
+    title = "Setting TCP/IP "
+    fieldNames = ["IP","Port" ]
+    fieldValues = ["localhost", "502"]  # we start with blanks for the values
+    fieldValues = easygui.multenterbox(msg,title, fieldNames, fieldValues)
 
-    print "asd"
-    mensaje = "\x01\x00\x00\x00\x00\x06\x01\x03\x00\x00\x00\x02"
-    s.send(mensaje)
-    print print_hex(s.recv(12))
+    ip = fieldValues[0]
+    port = int(fieldValues[1])
+    print ip
+    print port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, port))
+
+    # EJEMPLO DE COMUNICACION TCP
+    #mensaje = "\x01\x00\x00\x00\x00\x06\x01\x03\x00\x00\x00\x02"
+    #s.send(mensaje)
+    #print print_hex(s.recv(12))
+
+    while(True) :
+        value_print = 0
+        sys.stdout = open('output', 'w')
+        title = "Sistema de Transmision de Datos"
+        msg = "MODBUS"
+        choices = ["Read Holding Register","Write Single Register", "Write Multiple Register"]
+        reply = easygui.buttonbox(msg,title, choices=choices)
+
+        if reply == "Read Holding Register":
+            fieldNames = ["ID","Starting address","Quantity of Registers"]
+            fieldValues = []  # we start with blanks for the values
+            fieldValues = easygui.multenterbox(msg,title, fieldNames)
+
+            id = int(fieldValues[0])
+            start_addr = int(fieldValues[1])
+            reg_quantity = float(fieldValues[2])
+            times = int(reg_quantity) // 125
+            if (reg_quantity/125 - times) > 0 and reg_quantity > 125 :
+                times += 1
+            elif reg_quantity < 125 :
+                times = 1
+            index_reg = start_addr
+            reg_restantes = reg_quantity
+            count = 1
+            while (times > 0) :
+                quantity = 0.0
+                if reg_restantes < 125 :
+                    quantity = reg_restantes
+                else :
+                    quantity = reg_restantes - (( (reg_quantity)/125 )  - count ) * 125
+                recv = struct.pack("B", id) + F_03 + struct.pack("!h",start_addr) + struct.pack("!h", quantity) #corregir paquete
+                recv_multiple_values(recv)
+                value_print +=1
+                start_addr = start_addr +  125
+                reg_restantes -= quantity
+                times -= 1
+                count += 1
+
+        elif reply == "Write Single Register":
+            fieldNames = ["ID","Register Address","Register Value"]
+            fieldValues = []  # we start with blanks for the values
+            fieldValues = easygui.multenterbox(msg,title, fieldNames)
+
+            id = int(fieldValues[0])
+            start_addr = int(fieldValues[1])
+            reg_value = int(fieldValues[2])
+
+            send = struct.pack("B", id) + F_06 + struct.pack("!h",start_addr) + struct.pack("!H", reg_value) #corregir paquete
+            send_value(send) #arreglar funcion para usar tcp/ip
+
+
+        elif reply == "Write Multiple Register":
+            fieldNames = ["ID","Starting Address","Quantity of Registers", "Byte count", "Registers Value"]
+            fieldValues = []  # we start with blanks for the values
+            fieldValues = easygui.multenterbox(msg,title, fieldNames)
+
+            id = int(fieldValues[0])
+            start_addr = int(fieldValues[1])
+            reg_quantity = int(fieldValues[2])
+            byte_count = int(fieldValues[3])
+            reg_value = fieldValues[4].split(' ')
+
+            send_multiple = struct.pack("B", id) + "\x10" + struct.pack("!h",start_addr) + struct.pack("!h", reg_quantity) + struct.pack("B", byte_count) #corrregit estructura pquete
+
+            for value in range(0, reg_quantity):  # FUNCIONA CON 123 pero con 124 y 125 no
+                send_multiple = send_multiple + struct.pack("!H", int(reg_value[value]))
+            write_multiple_values(send_multiple)    #corregir funcion para tcp
+
+
+        sys.stdout = open ('output', 'r')
+        easygui.codebox(msg, title, sys.stdout.readlines())
 
 elif reply == "Serial Port":
-
 
     ser = serial.Serial()
 
     ports = list(serial.tools.list_ports.comports())
-
 
     msg = "Select the port to use"
     title = "Selecting Serial Port"
@@ -253,4 +416,4 @@ elif reply == "Serial Port":
 else :
     print "Incorrect option"
 
-
+exit()
